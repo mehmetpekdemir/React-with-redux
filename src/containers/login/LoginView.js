bf3239cef557154ecaca31e5bf9a423fdaf50438
Mehmet <mehmetpekdemir06@hotmail.com>
2021-02-06 02:26:00 +0300

    Project template.

---
+++ src/containers/login/LoginView.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoginView = () => {
+  return (
+    <div>
+      <h5>Login</h5>
+    </div>
+  );
+};
+export default LoginView;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 
 //Containers
 import HomeContainer from "../containers/home/HomeContainer";
+import LoginContainer from "../containers/login/LoginContainer";
 
 export const Routes = () => {
   return (
     <div>
       <Switch>
         <Route path="/" exact component={HomeContainer}></Route>
+        <Route path="/login" exact component={LoginContainer}></Route>
       </Switch>
     </div>
   );
